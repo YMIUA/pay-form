364a77c6b8b4349b4843af4b24ad284f200d67b6
Mykola Yatsuk <mykolayatsuk@Mykolas-MacBook-Pro.local>
2020-08-13 11:54:59 +0300

    Updates

---
+++ precache-manifest.4870c478388b034ed3907790de9b475a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14c4368241ec094961e0445956cad6dd",
+    "url": "/pay-form/index.html"
+  },
+  {
+    "revision": "aa901a4acc6a4d85474a",
+    "url": "/pay-form/static/css/main.3a4bbd11.chunk.css"
+  },
+  {
+    "revision": "be15bc984406132ab050",
+    "url": "/pay-form/static/js/2.68adde30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pay-form/static/js/2.68adde30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa901a4acc6a4d85474a",
+    "url": "/pay-form/static/js/main.b266288b.chunk.js"
+  },
+  {
+    "revision": "63412218dd76c83d1ade",
+    "url": "/pay-form/static/js/runtime-main.1cff6829.js"
+  },
+  {
+    "revision": "5c9f5798ab1f34d92163dc2dad77c06c",
+    "url": "/pay-form/static/media/close.5c9f5798.png"
+  },
+  {
+    "revision": "93e4eddf2d8ffd6557b02dcd01f6d04c",
+    "url": "/pay-form/static/media/footerImage.93e4eddf.png"
+  },
+  {
+    "revision": "eb852f812cd31987768b75e4d9029f9b",
+    "url": "/pay-form/static/media/kastaPay.eb852f81.png"
+  },
+  {
+    "revision": "1539fc5030b31f396f3d106a14fef889",
+    "url": "/pay-form/static/media/logo.1539fc50.png"
+  }
+]);
